1f8aadfce4c1bc2472b92fef722ae755946f1608
Beverly Lau <beverly.lau@klarna.com>
2018-03-15 19:39:15 +0100

    Add letters for BINGO, randomize

--- bingolotto.py
+++ bingolotto.py
@@ -1,13 +1,25 @@
-
-
 from flask import Flask,render_template,request,redirect
+import random
+
 app = Flask(__name__)
 NUMBERS = [1, 3, 13, 16, 22, 28, 30, 38, 39, 40, 43, 45, 46, 47, 55, 57, 59, 61, 64, 70, 72, 73, 75]
 
 @app.route('/index',methods=['GET','POST'])
 def index():
+    random.shuffle(NUMBERS)
     number = NUMBERS.pop()
-    return render_template('main.html',number=number)
+    if number <= 15:
+        letter = 'B'
+    elif number > 15 and number <= 30:
+        letter = 'I'
+    elif number > 30 and number <= 45:
+        letter = 'N'
+    elif number > 45 and number <= 60:
+        letter = 'G'
+    else:
+        letter = 'O'
+    
+    return render_template('main.html',number=letter + str(number))
 
 if __name__ == "__main__":
     app.run()
